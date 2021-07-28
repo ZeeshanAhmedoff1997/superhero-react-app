4c26915a4f69d1c802f64eb09b7071f4daf673a7
zeshan <zeshanahmedoff@gmail.com>
2021-07-29 00:08:41 +0500

    cors error

--- src/Services/searchService.js
+++ src/Services/searchService.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
-import { API_URL_SUFFIX, ACCESS_TOKEN } from "../Constants/constants";
+import {API_URL , API_URL_SUFFIX, ACCESS_TOKEN } from "../Constants/constants";
 
-const apiEndpoint = API_URL_SUFFIX + ACCESS_TOKEN  + "/search/";
+const apiEndpoint = API_URL + API_URL_SUFFIX + ACCESS_TOKEN  + "/search/";
 const jsonHeadersConfig = {
   headers: {
     "Content-Type": "application/json",
